53514d431366eed7e439f06a7106679aa7e178aa
westernat <westernat233@163.com>
2025-01-21 10:31:59 +0800

    贴图更新

--- src/main/java/org/confluence/mod/client/renderer/gui/WorkshopScreen.java
+++ src/main/java/org/confluence/mod/client/renderer/gui/WorkshopScreen.java
@@ -47,8 +47,10 @@ public class WorkshopScreen extends AbstractContainerScreen<WorkshopMenu> {
             this.upItem = null;
             this.downItem = null;
         }
-        String text = menu.getRecipesAmount() == 0 ? "0/0" : menu.getCurrentIndex() + 1 + "/" + menu.getRecipesAmount();
-        pGuiGraphics.drawString(font, text, leftPos + 144, topPos + 37 + (16 - font.lineHeight) / 2, 4210752, false);
+        if (menu.getRecipesAmount() > 0) {
+            String text = menu.getCurrentIndex() + 1 + "/" + menu.getRecipesAmount();
+            pGuiGraphics.drawString(font, text, leftPos + 144, topPos + 37 + (16 - font.lineHeight) / 2, 4210752, false);
+        }
     }
 
     @Override
@@ -56,9 +58,9 @@ public class WorkshopScreen extends AbstractContainerScreen<WorkshopMenu> {
         renderBackground(pGuiGraphics);
         pGuiGraphics.blit(BACKGROUND, leftPos, topPos, 0, 0, imageWidth, imageHeight);
         if (upButtonClicked) {
-            pGuiGraphics.blit(BACKGROUND, leftPos + 128, topPos + 25, 177, 0, 10, 7);
+            pGuiGraphics.blit(BACKGROUND, leftPos + 128, topPos + 23, 177, 0, 10, 8);
         } else if (downButtonClicked) {
-            pGuiGraphics.blit(BACKGROUND, leftPos + 128, topPos + 54, 177, 8, 10, 7);
+            pGuiGraphics.blit(BACKGROUND, leftPos + 128, topPos + 54, 177, 8, 10, 9);
         }
     }
 
@@ -99,7 +101,7 @@ public class WorkshopScreen extends AbstractContainerScreen<WorkshopMenu> {
     }
 
     private static boolean isOverUpButton(int x, int y) {
-        return x >= 128 && x <= 138 && y >= 25 && y <= 32;
+        return x >= 128 && x <= 138 && y >= 23 && y <= 30;
     }
 
     private static boolean isOverDownButton(int x, int y) {
